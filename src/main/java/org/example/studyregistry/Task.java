31281316f7f49c97cf01508a320f85a0583754e0
Mariana Hofer <marianahhofer@gmail.com>
2025-01-04 20:50:09 -0300

    fix: refactor code smell Task DataClass using Encapsulate Behavior refactoring technique

--- src/main/java/org/example/studyregistry/Task.java
+++ src/main/java/org/example/studyregistry/Task.java
@@ -1,8 +1,9 @@
 package org.example.studyregistry;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-public class Task extends Registry{
+public class Task extends Registry {
     private String title;
     private String description;
     private String author;
@@ -15,27 +16,58 @@ public class Task extends Registry{
         this.author = author;
         this.date = date;
     }
+
+    // Encapsulating behavior related to date formatting and manipulation
+    public String getFormattedDate() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return this.date.format(formatter);
+    }
+
+    public boolean isDueBefore(LocalDateTime otherDate) {
+        return this.date.isBefore(otherDate);
+    }
+
+    public boolean isDueAfter(LocalDateTime otherDate) {
+        return this.date.isAfter(otherDate);
+    }
+
+    public void postponeByDays(int days) {
+        this.date = this.date.plusDays(days);
+    }
+
+    public void advanceByDays(int days) {
+        this.date = this.date.minusDays(days);
+    }
+
+    // Getters and setters for fields
     public String getTitle() {
         return title;
     }
+
     public void setTitle(String title) {
         this.title = title;
     }
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
+
     public String getAuthor() {
         return author;
     }
+
     public void setAuthor(String author) {
         this.author = author;
     }
+
     public LocalDateTime getDate() {
         return date;
     }
+
     public void setDate(LocalDateTime date) {
         this.date = date;
     }
