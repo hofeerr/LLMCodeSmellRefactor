7d7cd3f2a671f69cfcd1541172044d1329b50689
Mariana Hofer <marianahhofer@gmail.com>
2025-01-04 22:01:12 -0300

    fix: refactor code smell StudyMaterial Long Method using Extract Method refactoring technique

--- src/main/java/org/example/studyregistry/StudyMaterial.java
+++ src/main/java/org/example/studyregistry/StudyMaterial.java
@@ -58,29 +58,48 @@ public class StudyMaterial{
         return response;
     }
 
-    public Map<String, Integer> getReferenceCountMap(){
-        Map<String, Integer> response = new HashMap<>();
-        response.put("Audio References", 0);
-        response.put("Video References", 0);
-        response.put("Text References", 0);
+    public Map<String, Integer> getReferenceCountMap() {
+        Map<String, Integer> response = initializeReferenceCountMap();
         for (Reference reference : references) {
-            if (reference.getClass() == AudioReference.class) {
-                Integer audioCount = response.get("Audio References");
-                response.put("Audio References", audioCount + 1);
-            } else if (reference.getClass() == VideoReference.class) {
-                if(((VideoReference) reference).handleStreamAvailability()){
-                    Integer videoCount = response.get("Video References");
-                    response.put("Video References", videoCount + 1);
-                }
-            } else if (reference.getClass() == TextReference.class){
-                if(((TextReference) reference).handleTextAccess()){
-                    Integer textCount = response.get("Text References");
-                    response.put("Text References", textCount + 1);
-                }
-            }
+            updateReferenceCount(reference, response);
         }
         setReferenceCount(response);
         return response;
+    }
+
+    private Map<String, Integer> initializeReferenceCountMap() {
+        Map<String, Integer> referenceCountMap = new HashMap<>();
+        referenceCountMap.put("Audio References", 0);
+        referenceCountMap.put("Video References", 0);
+        referenceCountMap.put("Text References", 0);
+        return referenceCountMap;
+    }
+
+    private void updateReferenceCount(Reference reference, Map<String, Integer> referenceCountMap) {
+        if (reference.getClass() == AudioReference.class) {
+            incrementCount("Audio References", referenceCountMap);
+        } else if (reference.getClass() == VideoReference.class) {
+            handleVideoReference(reference, referenceCountMap);
+        } else if (reference.getClass() == TextReference.class) {
+            handleTextReference(reference, referenceCountMap);
+        }
+    }
+
+    private void handleVideoReference(Reference reference, Map<String, Integer> referenceCountMap) {
+        if (((VideoReference) reference).handleStreamAvailability()) {
+            incrementCount("Video References", referenceCountMap);
+        }
+    }
+
+    private void handleTextReference(Reference reference, Map<String, Integer> referenceCountMap) {
+        if (((TextReference) reference).handleTextAccess()) {
+            incrementCount("Text References", referenceCountMap);
+        }
+    }
+
+    private void incrementCount(String key, Map<String, Integer> referenceCountMap) {
+        int currentCount = referenceCountMap.get(key);
+        referenceCountMap.put(key, currentCount + 1);
     }
 
 }
